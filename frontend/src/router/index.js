f2c6ac1eef1be15e29f7659ad7eeead35e8ae9a9
Tobias Chielens <tobias.chielens@gmail.com>
2020-07-23 09:07:51 +0200

    Redirect to login when jwt is not valid

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -128,11 +128,7 @@ router.beforeEach((to, from, next) => {
 
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!store.getters.isLogged) {
-      next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
-
+      window.location.replace(`${window.origin}/login`)
       return
     }
 
